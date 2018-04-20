22553a017d44c68cda9665e6455206e4d1e1bad0
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-04-20 19:02:40 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box
+}
+body {
+  font-family: "Lato", sans-serif;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+      <h2>About</h2>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem quis, aut optio doloribus magnam natus unde! Enim, ipsum. Quas architecto veniam sint exercitationem tenetur nobis, nihil porro provident autem tempore.</p>
+  </div>
+</template>
