d395c62428ee2c37de0eeefb149fef23b14d3a32
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-04-20 20:06:55 +0200

    update project  frontend

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <nuxt/>
+    <TheHeader />
+    <main class="main-content">
+      <nuxt/>
+    </main>
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/TheHeader/TheHeader.vue"
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+
+</script>
 
 <style>
 * {
   box-sizing: border-box
 }
+
 body {
   font-family: "Lato", sans-serif;
+}
+
+.main-content {
+  margin-top: 4.5rem;
 }
 </style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-      <h2>About</h2>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem quis, aut optio doloribus magnam natus unde! Enim, ipsum. Quas architecto veniam sint exercitationem tenetur nobis, nihil porro provident autem tempore.</p>
+  <div class="about-content">
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem quis, aut optio doloribus magnam natus unde! Enim, ipsum. Quas architecto veniam sint exercitationem tenetur nobis, nihil porro provident autem tempore.</p>
   </div>
 </template>
+
+<style scoped>
+  .about-content {
+    padding: 20px
+  }
+</style>
