530d0cd7520db54dedddfde290a08e400832b7c3
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-04-21 03:18:35 +0200

    config about page with backend

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="about-content">
-    <h2>About</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem quis, aut optio doloribus magnam natus unde! Enim, ipsum. Quas architecto veniam sint exercitationem tenetur nobis, nihil porro provident autem tempore.</p>
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
   </div>
 </template>
 
+<script>
+export default {
+  asyncData(context) {
+    return context.app.$storyapi.get('cdn/stories/about', {
+        version: 'draft'
+    })
+    .then(res => {
+      // console.log(res.data.story.content)
+      return {
+        title: res.data.story.content.title,
+        content: res.data.story.content.content
+      }
+    })
+  }
+}
+</script>
+
 <style scoped>
   .about-content {
-    padding: 20px
+    padding: 20px;
+    width: 80%;
+    max-width: 500px;
+    margin: auto;
   }
+  .about-content p {
+  white-space: pre-line;
+}
 </style>
